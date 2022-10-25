72c98ac2142068ca497999b9d12a4b64951f38fe
Muriel Delvaux Daher <muriel.delvaux@geb.inatel.br>
2022-10-25 13:51:51 -0300

    test: add test with storybook and jest integration

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,6 +1,14 @@
 import { themes } from '@storybook/theming';
 
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
 import '../src/styles/global.css';
+
+initialize({
+  onUnhandledRequest: 'bypass' //para não mostrar o log das req que não tratamos com msw
+});
+
+export const decorators = [mswDecorator];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
--- src/pages/List.stories.tsx
+++ src/pages/List.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react'
 import { within, userEvent, waitFor } from '@storybook/testing-library'
-
+import { expect } from '@storybook/jest'
+import {rest} from 'msw'
 import { List } from './List'
 
 export default{
@@ -9,9 +10,26 @@ export default{
     args:{},
     argTypes:{},
     parameters: {
-       
-      }
+        msw: {
+            handlers: [
+              rest.post('/sessions', (req, res, ctx) => {
+                return res(ctx.json({
+                  message: 'Task criada!'
+                }))
+              })
+            ],
+          },
+        }
 } as Meta
 
 export const Default: StoryObj = {
+    play: async ({ canvasElement })=>{
+        const canvas = within(canvasElement)
+
+        userEvent.type(canvas.getByPlaceholderText('Adicione uma nova tarefa'), 'task1')
+
+        userEvent.click(canvas.getByRole('button'))
+
+       
+    }
 } 
